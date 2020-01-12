6d7466130a80a54a3d25955d3a4633509f54efb6
xserverinternet <oliver.heilig@ptvgroup.com>
2020-01-12 23:59:54 +0100

    Token update

---
+++ token.js
@@ -0,0 +1,3 @@
+// The token below is for internal use only - consider getting your own token!
+// This token is valid until 2020-04-03T20:39:18Z and was auto-generated @ 2020-01-12T22:39:19Z.
+var token = 'AD820FE7-3DFF-43EE-B4AB-0DECAA96889B';
