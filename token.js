9741e8a1ec118057dc8c5a740ba4e63c10f3334c
xserverinternet <oliver.heilig@ptvgroup.com>
2020-05-01 23:57:21 +0200

    Token update

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
-// This token is valid until 2020-06-26T19:39:09Z and was auto-generated @ 2020-04-30T21:39:10Z.
-var token = '2D94C8C5-483A-4EBE-A078-95E7E3A88194';
+// This token is valid until 2020-07-24T21:39:08Z and was auto-generated @ 2020-05-01T21:39:09Z.
+var token = '682A4DEB-714F-4AA5-B112-73097878CAA5';
